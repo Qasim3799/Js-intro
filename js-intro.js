6b7fe98a30c26ba5064574c63bfeb98bab3964a3
Qasim3799 <qasim.adebisik@gmail.com>
2025-09-30 11:12:54 +0100

    Add files via upload

---
+++ js-intro.js
@@ -0,0 +1,63 @@
+// Strings
+
+console.log('Bolanle')
+
+console.log(typeof 'Bolanle')
+
+// Ways of creating strings in JavaScript
+// 1. Using single quotation marks (' ')
+// 2. Using double quotation marks (" ")
+// 3. Using back-ticks   (` `)
+
+console.log(typeof "Bolanle")
+
+console.log(typeof `Bolanle`)
+
+// Numbers
+// Types of numbers
+// 1. Integers e.g. 2, 5, 6, -3, 1
+// 2. Floats e.g. 2.12, -3.4, 1.0
+
+console.log(typeof 3)
+console.log(typeof 3.2)
+
+
+// Operators in JavaScripts
+// 1. Addition +
+// 2. Subtraction -
+// 3. Multiplication *
+// 4. Division /
+// 5. Power  **
+// 6. Modulo %  (returns the remainder of the division between two numbers)
+
+// console.log(4/2)
+// console.log(2 ** 4)  // power
+// console.log(4 % 2)
+// console.log(10 % 3)
+
+// alert(1 - 1 + 1 * 1 / 1) // BODMAS
+
+// D/M
+// A/S
+
+// alert('My name is Bolanle')
+
+// person = prompt('What is your name?')
+// console.log(person)
+
+
+// Converting from float to integer
+console.log(parseInt(3.2))
+console.log(parseInt('34'))
+
+console.log(typeof('3.45'))
+
+console.log(parseFloat('3.45'))
+console.log(parseFloat('34'))
+
+
+
+
+
+
+ 
